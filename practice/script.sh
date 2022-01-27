5e8a7175263692ff168b1981d73d3bd58520147f
Javid Mougamadou <javidjms0@gmail.com>
2022-01-27 21:55:33 +0100

    🔨 Add script.sh

---
+++ practice/script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+python main.py inputs/a_an_example.in.txt outputs/a_an_example.out.txt
+python main.py inputs/b_basic.in.txt outputs/b_basic.out.txt
+python main.py inputs/c_coarse.in.txt outputs/c_coarse.out.txt
+python main.py inputs/d_difficult.in.txt outputs/d_difficult.out.txt
+python main.py inputs/e_elaborate.in.txt outputs/e_elaborate.out.txt
