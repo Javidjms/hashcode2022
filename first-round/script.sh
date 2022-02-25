132a3ac523b70d82cef24362757b36a3b077a0f7
Javid Mougamadou <javidjms0@gmail.com>
2022-02-25 10:12:35 +0100

    🔨 Add script.sh

---
+++ first-round/script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+python main.py inputs/a_an_example.in.txt outputs/a_an_example.in.txt
+python main.py inputs/b_better_start_small.in.txt outputs/b_better_start_small.in.txt
+python main.py inputs/c_collaboration.in.txt outputs/c_collaboration.in.txt
+python main.py inputs/d_dense_schedule.in.txt outputs/d_dense_schedule.in.txt
+python main.py inputs/e_exceptional_skills.in.txt outputs/e_exceptional_skills.in.txt
+python main.py inputs/f_find_great_mentors.in.txt outputs/f_find_great_mentors.in.txt
